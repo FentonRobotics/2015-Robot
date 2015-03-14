f67777b09b98eafacbc8c0849c7abd30a5407066
Tom McAnnally <tom.mcannally@gmail.com>
2015-03-14 11:30:50 -0400

    Fix swapped robotbuilder

--- FentonRobotBuilderJava/src/org/usfirst/frc5114/FentonRobotBuilderJava/commands/StopBinArm.java
+++ FentonRobotBuilderJava/src/org/usfirst/frc5114/FentonRobotBuilderJava/commands/StopBinArm.java
@@ -35,7 +35,7 @@ public class  StopBinArm extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.binArm.stop();
+    	Robot.binArm.eh.cushyStop();
     }
 
     // Make this return true when this Command no longer needs to run execute()
